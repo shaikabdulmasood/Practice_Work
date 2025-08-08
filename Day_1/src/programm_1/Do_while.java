f17ecf4920976a60031ed9653db9c09a267e9d94
shaik Abdul Masood <shaikmasoodahammed12@gmail.com>
2025-08-08 10:53:23 +0530

    Initial commit

---
+++ Day_1/src/programm_1/Do_while.java
@@ -0,0 +1,18 @@
+package programm_1;
+
+public class Do_while {
+
+	public static void main(String args[])
+	{	
+		String [] Fruits = {"Mango","Apple","Banana","Kivi"};
+		int [] Price = {60,50,10,40};
+		System.out.println("Fruuits are ");
+		for(int i = 0; i < Fruits.length ; i++)
+		{
+			System.out.println(Fruits[i]+" cost is "+Price[i]);
+		}
+		
+	}
+
+
+}
